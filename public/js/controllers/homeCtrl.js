a19c5485090f1392c59332c14d81cb72ebac3be5
Austin Bourdier <abourdier@DB-MB70.local>
2015-10-08 15:06:35 -0700

    more changes

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,9 +1,9 @@
 angular.module('myApp')
 .controller('homeCtrl', homeController)
 .directive('onFinishRender', onFinishRender);
+
 function homeController($scope){
   $scope.cards = data;
-
   var stack;
   $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
     stack = gajus.Swing.Stack();
@@ -12,33 +12,73 @@ function homeController($scope){
       targetElement.classList.add('in-deck');
     });
     stack.on('throwoutleft', function (e) {
+      $scope.cards.pop();
       e.target.classList.remove('in-deck');
+      e.target.style.display = 'none';
     });
     stack.on('throwoutright', function (e) {
       $scope.match = true;
+      $scope.currentMatch = $scope.cards.pop();
       $scope.$apply();
       e.target.classList.remove('in-deck');
+      e.target.style.display = 'none';
     });
     stack.on('dragmove', function (e) {
       if(e.throwDirection == 1) {
         // right drag
-        $scope.draggingLeft = false;
-        $scope.draggingRight = true;
+        e.target.children[3].children[1].style.display = "none";
+        e.target.children[3].children[0].style.display = "inline";
       } else {
         // left drag
-        $scope.draggingRight = false;
-        $scope.draggingLeft = true;
+        e.target.children[3].children[0].style.display = "none";
+        e.target.children[3].children[1].style.display = "inline";
       }
-      $scope.$apply();
     });
     stack.on('dragend', function (e) {
-      $scope.draggingRight = false;
-      $scope.draggingLeft = false;
-      $scope.$apply();
+      // remove like/nope buttons
+      e.target.children[3].children[0].style.display = "none";
+      e.target.children[3].children[1].style.display = "none";
     });
   });
-
+  $scope.leaveMatchScreen = function() {
+    $scope.match = false;
+    $scope.currentMatch = null;
+  }
+  $scope.gamePadLeft = function(e) {
+    var topCard = document.getElementById('cardStack');
+    var children = topCard.children;
+    for(var index = children.length-1; index >= 0; index--) {
+      var classes = children[index].classList;
+      for(var classIndex = 0; classIndex < classes.length; classIndex++) {
+        if(classes[classIndex] == 'in-deck') {
+          var card = children[index];
+          card.classList.remove('in-deck');
+          card.style.display = 'none';
+          $scope.cards.pop();
+          return;
+        }
+      }
+    }
+  }
+  $scope.gamePadRight = function(e) {
+    var topCard = document.getElementById('cardStack');
+    var children = topCard.children;
+    for(var index = children.length-1; index >= 0; index--) {
+      var classes = children[index].classList;
+      for(var classIndex = 0; classIndex < classes.length; classIndex++) {
+        if(classes[classIndex] == 'in-deck') {
+          var card = children[index];
+          card.classList.remove('in-deck');
+          card.style.display = 'none';
+          $scope.match = true;
+          $scope.currentMatch = $scope.cards.pop();
+          return;
+        }
+      }
+    }
+  }
 }
+
 
 function onFinishRender ($timeout) {
   return {
